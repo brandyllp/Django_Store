5a73a97e5762c8522a9ed33fad0b61aad1a5ea06
brandyllp <brandyllp11@gmail.com>
2020-12-24 23:42:27 -0500

    Agregando Url

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+def myfirtsview(request):
+    data = {
+         'name': 'William'
+    }
+    return JsonResponse(data)
