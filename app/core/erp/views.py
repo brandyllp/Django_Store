aafc01be135ca9e49464b1bc30bf7abe04d9e1ac
brandyllp <brandyllp11@gmail.com>
2020-12-25 00:51:15 -0500

    configure template path

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def myfirtsview(request):
-    data = {
-         'name': 'William'
-    }
-    return JsonResponse(data)
+    data ={'name':'andres'}
+    return render(request, 'index.html', data)
